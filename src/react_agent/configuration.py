3aa1df3f02f426c821b4e834272453f3418bc948
mrxsierra <sunilsharma9@outlook.com>
2025-02-28 16:04:00 +0530

    change model and evn variables

--- src/react_agent/configuration.py
+++ src/react_agent/configuration.py
@@ -23,7 +23,8 @@ class Configuration:
     )
 
     model: Annotated[str, {"__template_metadata__": {"kind": "llm"}}] = field(
-        default="anthropic/claude-3-5-sonnet-20240620",
+        default="mistralai/ministral-large-latest",
+        # default="google_genai/gemini-2.0-flash",
         metadata={
             "description": "The name of the language model to use for the agent's main interactions. "
             "Should be in the form: provider/model-name."
